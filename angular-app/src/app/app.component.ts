72270fbc49cca74e7575fae90715bdf61177fe89
plowiecm <plowiecmagdalena@gmail.com>
2019-04-07 17:42:33 +0200

    complete view without results

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -27,21 +27,6 @@ export class AppComponent implements AfterViewInit {
   title = 'app works!';
 
   ngAfterViewInit() {
-    $('.nav a').on('click', function(){
-      $('.nav').find('.active').removeClass('active');
-      $(this).parent().addClass('active');
-    });
-
-    $('.dropdown').on('show.bs.dropdown', function(e){
-      $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
-    });
-
-    $('.dropdown').on('hide.bs.dropdown', function(e){
-      $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
-    });
-
-    $('.dropdown-menu li').on('click', function(){
-      $(this).parent().parent().addClass('active');
-    });
+  
   }
 }
